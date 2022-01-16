09648770cf244e39abfae389556e13386957775b
Martín Bonnefoy <mbonnefoy@uchile.cl>
2022-01-16 18:35:03 -0300

    Merge con herencias de HTML

--- AppProyecto1/urls.py
+++ AppProyecto1/urls.py
@@ -4,13 +4,20 @@ from django.urls.conf import include
 from AppProyecto1 import views
 
 urlpatterns = [
-    path('blogs/',views.blogs, name='Blog'),
-    path('comment/<id>',views.comment, name='Comment'),
-    path('showBlog/<id>',views.showBlog, name='ShowBlog'),
+    path('',views.inicio,name="Inicio"),
+    path('padre',views.padre, name="padre"),
+    # Esta es para ver la página padre
+    path('index',views.index, name="index"),
+    # Esta es para ver la planilla original completa
+    path('blogs',views.blogs, name="Blog"),
     
+    path('comment/<id>',views.comment, name="Comment"),
+    path('showBlog/<id>',views.showBlog, name="ShowBlog"),
+
     path("busquedaBlog/",views.busquedaBlog, name ="BusquedaBlog"),
     path("buscar/",views.buscar, name ="Buscar"),
-    path('blogForm/',views.blogForm, name='BlogForm'),
-    path('tagForm/',views.tagForm, name='TagForm'),
-    path('commentForm/',views.commentForm, name='CommentForm'),
+
+    path('blogForm/',views.blogForm, name="BlogForm"),
+    path('tagForm/',views.tagForm, name="TagForm"),
+    path('commentForm/',views.commentForm, name="CommentForm"),
 ]
