5c6e330d73a00eb2f37a3904682a064d4cc1ed8d
Florencia Mendoza <florladyuchiha@gmail.com>
2022-01-16 16:25:30 -0300

    Agrego metodo para ver los modelos por su nombre

--- AppProyecto1/models.py
+++ AppProyecto1/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 class Tag(models.Model):
     name=models.CharField(max_length=40)
+    def __str__(self):
+        return self.name
 
 class Blog(models.Model):
     title=models.CharField(max_length=40)
     subtitle=models.CharField(max_length=40)
     body=models.TextField()
     tag=models.ManyToManyField(Tag) #Este many to many crea la tabla blog_tag en la base
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     text=models.TextField()
