d2df8284a350328e0f215cd632131b9d773d53a7
Martín Bonnefoy <mbonnefoy@uchile.cl>
2022-01-16 12:24:38 -0300

    Estructura básica de herencias

--- AppProyecto1/urls.py
+++ AppProyecto1/urls.py
@@ -4,7 +4,14 @@ from django.urls.conf import include
 from AppProyecto1 import views
 
 urlpatterns = [
-    path('',views.blogs, name='Blog'),
-    path('comment/<id>',views.comment, name='Comment'),
-    path('showBlog/<id>',views.showBlog, name='ShowBlog'),
+    # path('',views.blogs, name='Blog'),
+    path('',views.inicio, name="inicio"),
+    path('padre',views.padre, name="padre"), # Esta es para ver la página padre
+    path('index',views.index, name="index"), # Esta es para ver la planilla original completa
+
+    path('blogs',views.blogs, name="blogs"),
+
+    path('showBlog/<id>',views.showBlog, name='showBlog'), # No funciona porque pide id
+    path('comment/<id>',views.comment, name='Comment'), # No funciona porque pide id
+
 ]
--- AppProyecto1/views.py
+++ AppProyecto1/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from django.shortcuts import render
+# from django.shortcuts import render
 from AppProyecto1.models import Blog, Tag, Comment
 
 
 def blogs(request):
     blogs=Blog.objects.all()
-    return render(request,'blogs.html',{"blogs":blogs})
+    return render(request, 'AppProyecto1/blogs.html',{"blogs":blogs})
 
 def comment(request,id):
     comment=Comment.objects.get(id=id)
-    return render(request, 'comment.html', {"comment":comment})
+    return render(request, 'AppProyecto1/comment.html', {"comment":comment})
 
 def showBlog(request,id):
     blog=Blog.objects.get(id=id)
     tags=Tag.objects.filter(blog__id=id)
-    return render(request, 'showBlog.html', {"blog":blog,"tags":tags})
+    return render(request, 'AppProyecto1/showBlog.html', {"blog":blog,"tags":tags})
+
+    # Páginas nuevas agregadas:
+
+def inicio(request):
+    # return render(request, 'AppProyecto1/index.html')
+    return render(request, 'AppProyecto1/inicio.html')
+
+def padre(request):
+    return render(request, 'AppProyecto1/padre.html')
+
+def index(request):
+    return render(request, 'AppProyecto1/index (plantilla vacía).html')
